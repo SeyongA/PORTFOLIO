08f413674c8d3b51f56b185f225e8f45071eda10
SeyongA <tlstpdyd11@naver.com>
2024-10-14 10:32:26 +0900

    수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -43,7 +43,6 @@ export default function App() {
   //   }
   // }, [currentSection]);
 
-
   return (
     <div
     //  onWheel={handleWheel} ref={contentRef} style={{ height: '100vh', overflow : 'hidden'}}
